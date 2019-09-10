596e8cb9495e777c4bfbc86ee4a2e2b6d7a5e492
Techno-Hwizrdry <alex.mardigian@gmail.com>
2019-09-10 16:14:36 -0400

    Updates for v3 of the HIBP API and one new feature.
    The HaveIBeenPwned API now requires an API key.  Details on that can be found at https://www.troyhunt.com/authentication-and-the-have-i-been-pwned-api . The API key is provided via a text file that the user needs to create and supply via the new -a switch.
    
    The new -n switch will return only the name of a breach.  If a user does not supply the -n switch, then the complete breach data will be returned.
    
    Added checks for 401 and 503 HTTP errors.
    
    Updated the README.md to reflect the necessity of supplying an API key via a text file.

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 This python script will check if a single email address, or a text file listing several email addresses, has been compromised in a data breach (pwned).  This script uses the haveibeenpwned API to compare the email address(es), provided by the user, to the haveibeenpwned database to check if they have been pwned or not.
 
 
+## API Key 
+[As of the HaveIBeenPwned v3](https://www.troyhunt.com/authentication-and-the-have-i-been-pwned-api/), you will need an API key to run checkpwnedemails.py.  You can get one [here](https://haveibeenpwned.com/API/Key).
+
+Once you have acquired an API key, make a new text file and put your API key in there.  Make sure this file has proper access permissions and do not share it.  checkpwnedemails.py will need to know the path of this file.  Use the -a switch to specify the file path.  Refer to the Usage section for examples.
+
+## Usage
+
 To check a single email address:
 
-python checkpwnedemails.py -s email_address
+`python checkpwnedemails.py -a path_to_API_key_file -s email_address`
 
 To check multiple email address:
 
-python checkpwnedemails.py -i text_file_listing_email_addresses
+`python checkpwnedemails.p -a path_to_API_key_filey -i text_file_listing_email_addresses`
 
 By default, the results will be printed to standard output.  However, if the -o option is provided, the output data will be printed to a tab delimited textfile for later use.
 
 For more options:
 
-python checkpwnedemails.py -h
+`python checkpwnedemails.py -h`
--- checkpwnedemails.py
+++ checkpwnedemails.py
@@ -1,5 +1,5 @@
 __author__  = "Alexan Mardigian"
-__version__ = "1.1.1"
+__version__ = "1.2"
 
 from argparse import ArgumentParser
 from time     import sleep
@@ -10,8 +10,13 @@ import traceback
 import urllib
 import urllib2
 
-PWNED_API_URL = "https://haveibeenpwned.com/api/v2/%s/%s"
-HEADERS = {"User-Agent": "checkpwnedemails"}
+
+HIBP_API_KEY  = "Your key here"
+PWNED_API_URL = "https://haveibeenpwned.com/api/v3/%s/%s?truncateResponse=%s"
+HEADERS = {
+           "User-Agent": "checkpwnedemails",
+           "hibp-api-key": "",
+}
 
 EMAILINDEX = 0
 PWNEDINDEX = 1
@@ -29,12 +34,14 @@ class PwnedArgParser(ArgumentParser):
 def get_args():
 	parser = PwnedArgParser()
 
+	parser.add_argument('-a', dest='apikey_path',  help='Path to text file that contains your HIBP API key.')
 	parser.add_argument('-b', action="store_true", dest='only_breaches', help='Return results for breaches only.')
 	parser.add_argument('-i', dest='input_path',   help='Path to text file that lists email addresses.')
+	parser.add_argument('-n', action="store_true", dest='names_only', help='Return the name of the breach(es) only.')
 	parser.add_argument('-o', dest='output_path',  help='Path to output (tab deliminated) text file.')
-        parser.add_argument('-p', action="store_true", dest='only_pwned', help='Print only the pwned email addresses.')
-        parser.add_argument('-s', dest="single_email", help='Send query for just one email address.')
-        parser.add_argument('-t', action="store_true", dest='only_pastebins', help='Return results for pastebins only.')
+	parser.add_argument('-p', action="store_true", dest='only_pwned', help='Print only the pwned email addresses.')
+	parser.add_argument('-s', dest="single_email", help='Send query for just one email address.')
+	parser.add_argument('-t', action="store_true", dest='only_pastebins', help='Return results for pastebins only.')
 
 	if len(sys.argv) == 1:  # If no arguments were provided, then print help and exit.
 		parser.print_help()
@@ -46,28 +53,32 @@ def get_args():
 def clean_list(list_of_strings):
 	return [str(x).strip() for x in list_of_strings]
 
-def get_results(email_list, service, opts):
+def get_results(email_list, service, opts, hibp_api_key):
 	results = []  # list of tuples (email adress, been pwned?, json data)
 
 	for email in email_list:
 		email = email.strip()
 		data = []
-                req  = urllib2.Request(PWNED_API_URL % (urllib.quote(service), urllib.quote(email)), headers=HEADERS)
+		names_only = "true" if opts.names_only else "false"
+		req = urllib2.Request(PWNED_API_URL % (urllib.quote(service), urllib.quote(email), urllib.quote(names_only)), headers=HEADERS)
 
-                try:
-                	response = urllib2.urlopen(req)  # This is a json object.
-                        data     = json.loads(response.read())
+		try:
+			response = urllib2.urlopen(req)  # This is a json object.
+			data     = json.loads(response.read())
 			results.append( (email, True, data) )
-
-                except urllib2.HTTPError as e:
-                        if e.code == 400:
+		except urllib2.HTTPError as e:
+			if e.code == 400:
 				print "%s does not appear to be a valid email address.  HTTP Error 400." % (email)
+			if e.code == 401:
+				print "Unauthorised - the API key provided (%s) was not valid." % (hibp_api_key)
 			if e.code == 403:
 				print "Forbidden - no user agent has been specified in the request.  HTTP Error 403."
-                        if e.code == 404 and not opts.only_pwned:
+			if e.code == 404 and not opts.only_pwned:
 				results.append( (email, False, data) )
 			if e.code == 429:
 				print "Too many requests; going over the request rate limit.  HTTP Error 429."
+			if e.code == 503:
+				print "Service unavailable."
 
 		sleep(1.6)  # This 1.6 second delay is for rate limiting.
 
@@ -152,32 +163,46 @@ def write_results_to_file(filename, results, opts):
 
 		outfile.write(file_headers[f] + '\n')
 
-        	for r in res:
+		for r in res:
 			outfile.write(tab_delimited_string(r) + '\n')
 
-        	outfile.close()
+		outfile.close()
 
 def main():
+	hibp_api_key = ""
 	email_list = []
 	opts = get_args()
+
+	if not opts.apikey_path:
+		print "\nThe path to the file containing the HaveIBeenPwned API key was not found."
+		print "Please provide the file path with the -a switch and try again.\n"
+		sys.exit(1)
+	else:
+		try:
+			with open(opts.apikey_path) as apikey_file:
+				hibp_api_key = apikey_file.readline().strip()
+				HEADERS["hibp-api-key"] = hibp_api_key
+		except IOError:
+			print "\nCould not read file:", opts.apikey_path
+			print "Check if the file path is valid, and try again.\n"
+			sys.exit(1)
 
 	if opts.single_email:
 		email_list = [opts.single_email]
 	else:
 		email_list_file = open(opts.input_path, 'r')
 		email_list      = clean_list(email_list_file.readlines())
-
 		email_list_file.close()
 
-        results = []
+	results = []
 
-        if opts.only_breaches:
-		results.append(get_results(email_list, BREACHED, opts))
+	if opts.only_breaches:
+		results.append(get_results(email_list, BREACHED, opts, hibp_api_key))
 	elif opts.only_pastebins:
-		results.append(get_results(email_list, PASTEBIN, opts))
+		results.append(get_results(email_list, PASTEBIN, opts, hibp_api_key))
 	else:
-		results.append(get_results(email_list, BREACHED, opts))
-		results.append(get_results(email_list, PASTEBIN, opts))
+		results.append(get_results(email_list, BREACHED, opts, hibp_api_key))
+		results.append(get_results(email_list, PASTEBIN, opts, hibp_api_key))
 
 	if opts.output_path:
 		write_results_to_file(opts.output_path, results, opts)
