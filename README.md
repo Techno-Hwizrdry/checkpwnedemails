044776cea316b34be17591ed1641868bcd3e98a4
Techno-Hwizrdry <alex.mardigian@gmail.com>
2020-01-15 15:43:22 -0500

    Updated to be compatible with Python 3
    checkpwnedemails.py can now run on both Python 2 and Python 3.
    In order to make it compatible with Python 3, all the print statements have been replaced with print() statements.
    Also, the urllib and urllib2 libraries have been replaced with the requests library.
    Reger to the 'Dependencies' section of README.md for more details.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # checkpwnedemails
-NOTE: This version of checkpwnedemails.py is made for Python 2. [Click here if you want the version that uses Python 3.](https://github.com/Techno-Hwizrdry/checkpwnedemails3)
 
 This python script will check if a single email address, or a text file listing several email addresses, has been compromised in a data breach (pwned).  This script uses the haveibeenpwned API to compare the email address(es), provided by the user, to the haveibeenpwned database to check if they have been pwned or not.
 
+## Dependencies
+To run this script you will need to have the [requests library](https://2.python-requests.org/projects/3/) installed.
+Also, you will need pip to install the requests library.  [pip is already installed if you are using Python 2 >=2.7.9 or Python 3 >=3.4.](https://pip.pypa.io/en/stable/installing/)
+
+To install requests, run the following command:
+`pip install requests`
+
+Or if you're using Python 3:
+
+`pip3 install requests`
 
 ## API Key 
 [As of the HaveIBeenPwned v3 update](https://www.troyhunt.com/authentication-and-the-have-i-been-pwned-api/), you will need an API key to run checkpwnedemails.py.  You can get one [here](https://haveibeenpwned.com/API/Key).
@@ -17,7 +26,7 @@ To check a single email address:
 
 To check multiple email address:
 
-`python checkpwnedemails.p -a path_to_API_key_filey -i text_file_listing_email_addresses`
+`python checkpwnedemails.py -a path_to_API_key_filey -i text_file_listing_email_addresses`
 
 By default, the results will be printed to standard output.  However, if the -o option is provided, the output data will be printed to a tab delimited textfile for later use.
 
--- checkpwnedemails.py
+++ checkpwnedemails.py
@@ -1,13 +1,12 @@
 __author__  = "Alexan Mardigian"
-__version__ = "1.2.1"
+__version__ = "1.2.3"
 
 from argparse import ArgumentParser
 from time     import sleep
 
 import json
+import requests
 import sys
-import urllib
-import urllib2
 
 
 PWNED_API_URL = "https://haveibeenpwned.com/api/v3/%s/%s?truncateResponse=%s"
@@ -66,9 +65,9 @@ def printHTTPErrorOutput(http_error_code, hibp_api_key, email=None):
 	}
 
 	try:
-		print ERROR_CODE_OUTPUT[http_error_code]
+		print(ERROR_CODE_OUTPUT[http_error_code])
 	except KeyError:
-		print "HTTP Error %s" % (http_error_code)
+		print("HTTP Error %s" % (http_error_code))
 
 	if http_error_code == 401:
 		sys.exit(1)
@@ -80,13 +79,22 @@ def get_results(email_list, service, opts, hibp_api_key):
 		email = email.strip()
 		data = []
 		names_only = "true" if opts.names_only else "false"
-		req = urllib2.Request(PWNED_API_URL % (urllib.quote(service), urllib.quote(email), urllib.quote(names_only)), headers=HEADERS)
 
 		try:
-			response = urllib2.urlopen(req)  # This is a json object.
-			data     = json.loads(response.read())
-			results.append( (email, True, data) )
-		except urllib2.HTTPError as e:
+			response = requests.get(url=PWNED_API_URL % (service, email, names_only), headers=HEADERS)
+			is_pwned = True
+
+			# Before parsing the response (for JSON), check if any content was returned.
+			# Otherwise, a json.decoder.JSONDecodeError will be thrown because we were trying
+			# to parse JSON from an empty response.
+			if response.content:
+				data = response.json()
+			else:
+				data = None   # No results came back for this email.  According to HIBP, this email was not pwned.
+				is_pwned = False
+
+			results.append( (email, is_pwned, data) )
+		except requests.exceptions.HTTPError as e:
 			if e.code == 404 and not opts.only_pwned:
 				results.append( (email, False, data) )  # No results came back for this email.  According to HIBP, this email was not pwned.
 			elif e.code != 404:
@@ -100,13 +108,13 @@ def get_results(email_list, service, opts, hibp_api_key):
 
 				if not last_result[PWNEDINDEX]:
 					if service == BREACHED:
-						print "Email address %s not pwned.  Yay!" % (email)
+						print("Email address %s not pwned.  Yay!" % (email))
 					else:
-						print "Email address %s was not found in any pastes.  Yay!" %(email)
+						print("Email address %s was not found in any pastes.  Yay!" %(email))
 				elif data:
-					print "\n%s pwned!\n==========" % (email)
-					print json.dumps(data, indent=4)
-					print '\n'
+					print("\n%s pwned!\n==========" % (email))
+					print(json.dumps(data, indent=4))
+					print('\n')
 
 			except IndexError:
 				pass
@@ -186,8 +194,8 @@ def main():
 	opts = get_args()
 
 	if not opts.apikey_path:
-		print "\nThe path to the file containing the HaveIBeenPwned API key was not found."
-		print "Please provide the file path with the -a switch and try again.\n"
+		print("\nThe path to the file containing the HaveIBeenPwned API key was not found.")
+		print("Please provide the file path with the -a switch and try again.\n")
 		sys.exit(1)
 	else:
 		try:
@@ -195,8 +203,8 @@ def main():
 				hibp_api_key = apikey_file.readline().strip()
 				HEADERS["hibp-api-key"] = hibp_api_key
 		except IOError:
-			print "\nCould not read file:", opts.apikey_path
-			print "Check if the file path is valid, and try again.\n"
+			print("\nCould not read file:", opts.apikey_path)
+			print("Check if the file path is valid, and try again.\n")
 			sys.exit(1)
 
 	if opts.single_email:
@@ -206,8 +214,8 @@ def main():
 		email_list      = clean_list(email_list_file.readlines())
 		email_list_file.close()
 	else:
-		print "\nNo email addresses were provided."
-	 	print "Please provide a single email address (using -s) or a list of email addresses (using -i).\n"
+		print("\nNo email addresses were provided.")
+		print("Please provide a single email address (using -s) or a list of email addresses (using -i).\n")
 		sys.exit(1)
 
 	results = []
